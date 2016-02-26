abd78a2f704242c23bbc00ac448f23b057cc0c89
Guilherme Moura <guime.sprint@gmail.com>
2016-02-25 17:23:42 -0800

    Added initial Project file.

---
+++ CropViewController/CropViewController.h
@@ -0,0 +1,19 @@
+//
+//  CropViewController.h
+//  CropViewController
+//
+//  Created by Guilherme Moura on 2/25/16.
+//  Copyright © 2016 Reefactor, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CropViewController.
+FOUNDATION_EXPORT double CropViewControllerVersionNumber;
+
+//! Project version string for CropViewController.
+FOUNDATION_EXPORT const unsigned char CropViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CropViewController/PublicHeader.h>
+
+
